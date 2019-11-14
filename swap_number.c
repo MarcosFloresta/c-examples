986df9b66aecbbe69e9eba09e75cb48ef8e58721
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-14 13:11:01 -0300

    Create swap_number.c

---
+++ swap_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    double firstNumber, secondNumber;
+    printf("Enter first number: ");
+    scanf("%lf", &firstNumber);
+    printf("Enter second number: ");
+    scanf("%lf", &secondNumber);
+    // Swapping process
+    firstNumber = firstNumber - secondNumber;
+    secondNumber = firstNumber + secondNumber;
+    firstNumber = secondNumber - firstNumber;
+    printf("\nAfter swapping, firstNumber = %.2lf\n", firstNumber);
+    printf("After swapping, secondNumber = %.2lf", secondNumber);
+    return 0;
+}
